52400f9386c2088744b45c514bb47552279bea58
Amanda Garcia <algarcian@outlook.com>
2019-06-07 12:10:53 -0600

    Changes

--- src/components/Effect.js
+++ src/components/Effect.js
@@ -1,15 +1,24 @@
-import React, {Fragment} from 'react'
+import React, {Fragment, useEffect, useState} from 'react'
 
 const Effect = (props) => {
+  const [mounted, setMounted] = useState(true)
 
+  const toggle = () => setMounted(!mounted)
   const goBack = () => props.history.goBack()
+
+  // useEffect wants to return an array so you can pass in a second argument of on array
+  // the array at the end is considered a dependency and will caused the mounted method to fire right away when the component renders. If it is passed in, useEffect with monitor the argument that is being passed in. 
+  useEffect(() => {
+    console.log('rendering mounted is', mounted)
+  }, [mounted])
 
   return (
   <Fragment>
   <div className="App">
     <header className="App-header">
-      <h1>Test</h1>
+      <button onClick={toggle}>Show/Hide Effect</button>
       <button onClick={() => goBack()}>Go Back</button>
+
     </header>
   </div>
   </Fragment>
