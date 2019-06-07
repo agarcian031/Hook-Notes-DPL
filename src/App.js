df837eab693ba83d891a102051b405692dc50942
Amanda Garcia <algarcian@outlook.com>
2019-06-07 11:56:58 -0600

    Fill in component data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Fragment} from 'react';
 import { Router, Switch, Route, Link} from 'react-router-dom'
+import Effect from './components/Effect'
 import State from './components/User'
 import './App.css';
 
@@ -7,11 +8,16 @@ const App = () => (
     <Fragment>
       <Switch>
       <Route exact path='/state' component={State}/>
+      <Route exact path='/effect' component={Effect}/>
       </Switch>
     <div className="App">
       <header className="App-header">
         <Link style={{color: 'green'}} to="/state">
           Try useState
+        </Link>
+        <div>-----</div>
+        <Link style={{color: 'green'}} to="/effect">
+          Try useEffect
         </Link>
       </header>
     </div>
---
+++ src/components/Effect.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Effect = () => (
+  <div className="App">
+    <header className="App-header">
+      <h1>Test</h1>
+    </header>
+  </div>
+)
+
+export default Effect
