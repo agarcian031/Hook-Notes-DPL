a618bd832ef4b84b8135622d8f98f506d26a533d
Amanda Garcia <algarcian@outlook.com>
2019-06-07 11:32:03 -0600

    Add react router and goBack statement

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Fragment} from 'react';
+import { Router, Switch, Route, Link} from 'react-router-dom'
+import State from './components/User'
 import './App.css';
 
-function App() {
-  return (
+const App = () => (
+    <Fragment>
+      <Switch>
+      <Route exact path='/state' component={State}/>
+      </Switch>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Link style={{color: 'green'}} to="/state">
+          Try useState
+        </Link>
       </header>
     </div>
-  );
-}
+    </Fragment>
+)
 
 export default App;
--- src/components/User.js
+++ src/components/User.js
@@ -1,14 +1,61 @@
-import React, { Component } from 'react'
+import React, { useState, Fragment  } from 'react'
+import '../App.css'
 
-class User extends Component {
-  render() {
-    return (
-      <div>
-        <h1>test</h1>
-      </div>
-    )
-  }
+// const User = () =>  {
+//   // When turning a class into a function, you can't use this. You can still have functional methods inside like a toggleForm() method but it has to be assigned to a variable and you won't need to use this or a callback() when passing it into props. You just call the variable name because you are storing a method into a variable. 
+
+  
+//   // Original 
+//   // state = { users: [], toggle: false, title: 'string'}
+
+//   // Using hooks. You can you more than one useState (hook) per variable or piece of state that you may need. 
+//   // don't need a component did mount. 
+//     const [ users, setUsers] = useState([])
+//     // const [ toggle, setToggle] = useState(false)
+//     // const [ title, setTitle] = useState('string')
+
+//     const updateUsers = () => {
+//       setUsers(someData)
+//     }
+
+
+
+
+//   // How react would interact with rails via axios api fetch 
+//   // will make a http request to the rails routes and populate the user object with data. 
+//   // componentDidMount() {
+//   //   axios.get('/api/rails_user_end_point')
+//   //   .then( res => {
+//   //     this.setState({ users: res.data })
+//   //   }) 
+//   // }
+//     return (
+//       <Fragment>
+//        <ul>
+//          {users.map( user => {
+//            <li>{user.name}</li>
+//          })}
+//        </ul>
+//       </Fragment>
+//     )
+// }
+
+// export default User
+
+// Called state because it will demonstrate the useState component. 
+
+const State = (props) => {
+  // built in goBack function given through react router. 
+  const goBack = () => props.history.goBack()
+
+  return ( 
+  <div className="App">
+      <header className="App-header">
+        <button onClick={() => goBack()}>Go Back</button>
+
+      </header>
+  </div>
+)
 }
 
-export default User
-
+export default State; 
