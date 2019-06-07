30bf5d1738523fb96453ba6747e653962bc4a6ad
Amanda Garcia <algarcian@outlook.com>
2019-06-07 11:59:05 -0600

    Go back button added

--- src/components/Effect.js
+++ src/components/Effect.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 
-const Effect = () => (
+const Effect = (props) => {
+
+  const goBack = () => props.history.goBack()
+
+  return (
+  <Fragment>
   <div className="App">
     <header className="App-header">
       <h1>Test</h1>
+      <button onClick={() => goBack()}>Go Back</button>
     </header>
   </div>
-)
+  </Fragment>
+  )
+}
 
 export default Effect
